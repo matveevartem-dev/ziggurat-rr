df392892c07cda6762f2f0007160286c356fee2a
Artem Matveev <dev.artem.matveev@gmail.com>
2026-06-08 18:47:12 +0300

    first commit

---
+++ src/worker-core.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * ROADRUNNER CORE WORKER
+ */
+
+use Spiral\RoadRunner;
+use Nyholm\Psr7;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+// Максимально примитивный цикл без внешних классов (только ядро RR)
+$worker = RoadRunner\Worker::create();
+$psrFactory = new Psr7\Factory\Psr17Factory();
+$psr7 = new RoadRunner\Http\PSR7Worker($worker, $psrFactory, $psrFactory, $psrFactory);
+
+while ($request = $psr7->waitRequest()) {
+    try {
+        $res = $psrFactory->createResponse(200)
+            ->withHeader('Content-Type', 'application/json; charset=utf-8');
+            
+        $res->getBody()->write(json_encode([
+            'status' => 'maintenance',
+            'message' => 'Zigguret OS Core-Api на техобслуживании...'
+        ], JSON_UNESCAPED_UNICODE));
+        
+        $psr7->respond($res);
+        
+    } catch (\Throwable $e) {
+        $worker->error((string)$e);
+    }
+}
